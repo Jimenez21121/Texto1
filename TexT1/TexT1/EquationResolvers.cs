1ce5450706cc9a20de9d257b61efe71cc3dc10e9
Francisco Jiménez Lozano 1-M <jimenez21121@iesmarenostrum.com>
2020-09-29 19:42:14 +0200

    Ecuacion man
    EquationResolvers

---
+++ TexT1/TexT1/EquationResolvers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TexT1
+{
+    class EquationResolvers
+    {
+        static public double FirstEquation(double a, double b)
+        {
+            double aux, x;
+            aux = b * -1;
+            x = (aux) / a;
+
+            return x;
+        }
+        static public double SecondEquation(double a, double b, double c)
+        {
+            double aux, aux2, x;
+            aux = b * b - 4 * a * c;
+            aux2 = System.Math.Sqrt(aux);
+            x = -(b) + (aux2) / 2 * a;
+
+            return x;
+
+        }
+    }
+}
--- TexT1/TexT1/Program.cs
+++ TexT1/TexT1/Program.cs
@@ -4,9 +4,10 @@ namespace TexT1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+         
         }
     }
 }

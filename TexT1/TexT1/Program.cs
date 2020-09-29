03b7cb85703fec62618c14d2aa27cf8b8346d725
Francisco Jiménez Lozano 1-M <jimenez21121@iesmarenostrum.com>
2020-09-29 19:19:05 +0200

    Prueba 2 commit
    exclamaciones

--- TexT1/TexT1/Program.cs
+++ TexT1/TexT1/Program.cs
@@ -6,7 +6,7 @@ namespace TexT1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
